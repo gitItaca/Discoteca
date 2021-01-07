eaca10f8fc619007264092a561064f750deb19a1
gitItaca <0314lsolanes@e-itaca.es>
2021-01-07 18:54:32 +0100

    FuncionValidarURLMetadata
    La validacion no está bien, no compruebo el principio, solo si es diferente a la URL, lo cual hace que de momento no tenga sentido.

--- src/main/java/DI2021/Discoteca/utils/AlbumUtils.java
+++ src/main/java/DI2021/Discoteca/utils/AlbumUtils.java
@@ -1,10 +1,22 @@
 package DI2021.Discoteca.utils;
 
+import java.util.List;
+
+import DI2021.Discoteca.models.Album;
+import DI2021.Discoteca.services.AlbumRecoveryService;
+
 public class AlbumUtils {
 	public static boolean ValidarURLMetadata(String metaInfo) {
+		List<Album> myAlbums = AlbumRecoveryService.getAllAlbums();
 		
-		
-		//Este método debe validar que las url de metadatos de los albums referencian 
+		for(Album album : myAlbums) {
+			//Arreglar condicion del if, tengo que comparar si es igual el comienzo, no todo.
+			if (album.getMetaInfo() != "https://en.wikipedia.org/wiki/") {
+				album.setMetaInfo("https://en.wikipedia.org/wiki/" + album.getMetaInfo());
+			}
+				
+		}
+		// debe validar que las url de metadatos de los albums referencian 
 		// al sitio web del disco en la wikipedia.
 		return (Boolean) null;
 	}
